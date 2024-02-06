beb89f1cdd7394a577501017adee151358d5864b
Onaconnapuna <cjgermain12@gmail.com>
2024-02-05 22:05:44 -0500

    added pieces, might reconsider building the pieces out this way

--- chess/src/piece.rs
+++ chess/src/piece.rs
@@ -1,7 +1,54 @@
 use crate::board::Board;
 
-pub struct Piece {
+// pub struct Piece {
+//     symbol: char,
+//     board: Board,
+//     pos: [u8;2],
+// }     
+
+// maybe make Pieces an enum?
+
+struct Pawn {
     symbol: char,
     board: Board,
-    pos: [u8;2],
-}     
+    pos: [u8;2], 
+}
+
+struct King {
+    symbol: char,
+    board: Board,
+    pos: [u8;2], 
+}
+
+struct Queen {
+    symbol: char,
+    board: Board,
+    pos: [u8;2], 
+}
+
+struct Bishop {
+    symbol: char,
+    board: Board,
+    pos: [u8;2], 
+}
+
+struct Knight {
+    symbol: char,
+    board: Board,
+    pos: [u8;2], 
+}
+
+struct Rook  {
+    symbol: char,
+    board: Board,
+    pos: [u8;2], 
+}
+
+enum Piece {
+    PawnPiece(Pawn),
+    KingPiece(King),
+    QueenPiece(Queen).
+    BishopPiece(Bishop),
+    KnightPiece(Knight),
+    RookPiece(Rook),
+}
