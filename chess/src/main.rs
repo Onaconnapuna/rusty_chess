bb40720f8264cbf82c54b96e5d7ff4cb1323bb73
Onaconnapuna <cjgermain12@gmail.com>
2024-10-22 22:48:01 -0400

    added boiler plate bevy app and entity/component, git ignored target dir

--- chess/src/main.rs
+++ chess/src/main.rs
@@ -5,9 +5,9 @@ pub mod piece;
 // Board needs to implement Board.move_piece(&self, start pos, end pos)
 // with only one mut reference what to we do with pieces.
 // how about all the pieces will hold a read reference to the board
-// the board will always change itself within its own function impl 
+// the board will always change itself within its own function impl
 
-fn main() {
+/*fn main() {
     let mut _board = board::Board {
        grid: [ [Piece { kind: PieceKind::Pawn, color: PieceColor::White }; 8]; 8]
     };
@@ -15,4 +15,29 @@ fn main() {
     // for row in _board.grid {
     //     println!("Row: {:?}", row);
     // }
+} */
+
+#[derive(Component)]
+struct Position {
+    x: f32,
+    y: f32,
 }
+
+struct Entity(u64);
+
+use bevy::prelude::*;
+
+fn hello_world() {
+    println!("hello world!");
+}
+
+fn main() {
+    App::new().add_systems(Update, hello_world).run();
+}
+
+fn print_position_system(query: Query<&Position>) {
+    for position in &query {
+        println!("position: {} {}", position.x, position.y);
+    }
+}
+
