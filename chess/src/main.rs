bcbfc1b9bf0bd864826fd6efdcc398e5696e9117
Onaconnapuna <cjgermain12@gmail.com>
2024-11-03 17:25:08 -0500

    Working on 2d camera

--- chess/src/main.rs
+++ chess/src/main.rs
@@ -1,11 +1,32 @@
 use bevy::prelude::*;
 
 #[derive(Component)]
+struct MyCameraMarker;
+
+#[derive(Component)]
 struct Position {
     x: i8,
     y: i8,
 }
 
 fn main() {
-    println!("Hello World!");
+    App::new()
+        .add_plugins(WindowPlugin {
+            primary_window: Some(Window {
+                resolution: (140.0, 140.0).into(),
+                title: "Rusty Chess".to_string(),
+                ..default()
+            }),
+            ..default()
+        })
+        .add_systems(Startup, camera_setup)
+        .run();
+}
+
+fn camera_setup(mut commands: Commands) {
+    commands.spawn((Camera2dBundle { ..default() }, MyCameraMarker));
+}
+
+fn debug_projection(query_camera: Query<&OrthographicProjection, With<MyCameraMarker>>) {
+    let projection = query_camera.single();
 }
